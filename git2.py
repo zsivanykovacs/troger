49e59041ebb5d9cd36f600a0505d9f4352072bfb
kovacs <zsivanytroger@gmail.de>
2025-03-18 14:05:07 +0100

    ando kremégy fejlesztések

--- git2.py
+++ git2.py
@@ -2,6 +2,9 @@ print("Halo itt Tessék!")
 a=5
 b=2
 print(a+b)
+a=int(input("Kérek egy egész számot:"))
+b=int(input("Kérek egy másik egész számot:"))
+print(f"Összegük: {a+b}")
 for i in range(1,4):
     print("Nemsoká vége a dalnak!")
 
